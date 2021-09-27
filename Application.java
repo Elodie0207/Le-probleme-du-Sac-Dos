3df2a65ec6d49c8d154075ecd1dc4a6cbb347f92
Meryem0207 <72689631+Meryem0207@users.noreply.github.com>
2021-09-27 22:20:33 +0200

    Update Application.java

--- Application.java
+++ Application.java
@@ -3,9 +3,8 @@ import objet.Objet;
 public class Application {
 
     public static void main(String[] args) {
-        Objet obj1 = new Objet("caca", 50.00f, 1.00f);
-        Objet obj2= new Objet("pipi", 10.00f, 6.00f);
-        System.out.println(obj1);
-        System.out.println(obj2);
+        String chemin = "scr/appli/items.txt";
+
+
     }
 }
