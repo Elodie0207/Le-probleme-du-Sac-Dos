438335c15ddc0d89acf2cca95c03cb94e2bed38e
Meryem0207 <72689631+Meryem0207@users.noreply.github.com>
2021-09-29 19:26:04 +0200

    Update Objet.java

--- objet/Objet.java
+++ objet/Objet.java
@@ -1,45 +1,53 @@
 package objet;
 
+import java.util.Comparator;
+
 public class Objet {
 
     private boolean estALinterieur;
     private String chemin;
     private String nom;
-    private float poids;
-    private float prix;
-    private float valeur;
-
+    private double poids;
+    private double prix;
+    private double rapport;
 
     //Constructeur d'Objet
-    public Objet(String nom, float poids, float prix){
+    public Objet(String nom, double poids, double prix){
         this.nom=nom;
         this.poids=poids;
         this.prix=prix;
+        this.rapport=prix/poids;
     }
 
 
 
     public String getNom() {
-        return nom;
+        return this.nom;
     }
 
-    public float getPoids() {
-        return poids;
+    public double getPoids() {
+        return this.poids;
     }
 
-    public float getPrix() {
-        return prix;
+    public double getPrix() {
+        return this.prix;
     }
-    public float getValeur() {
-        return valeur;
-    }
+
+
+    public double getRapport(){return this.rapport;}
     @Override
     public String toString() {
-        return "  " + nom +  " : "  + "poids "+ poids +  " ; "  + " valeur " + prix;
+        return "  " + nom +  " : "  + "poids "+ poids +  " ; "  + " prix " + prix;
     }
 
 
     public boolean EstALinterieur(){
         return true;
     }
+
+    public static Comparator <Objet> rapport() {
+        return ((o1, o2) -> Double.compare(o2.getRapport(),o1.getRapport()));
+    }
+}
+
 }
