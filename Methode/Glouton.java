56411d66466eca87578e63b977598d40bceb249e
Meryem0207 <72689631+Meryem0207@users.noreply.github.com>
2021-10-02 13:52:07 +0200

    Update Glouton.java

--- Methode/Glouton.java
+++ Methode/Glouton.java
@@ -2,53 +2,56 @@ package methode;
 
 import objet.Objet;
 
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
 
 import sac.SacADos;
-public class Glouton {
-    SacADos sac;
-    public Glouton(){
-        //changer arraylist -> tab
-        ArrayList<Objet> listeObjet = new ArrayList<>(sac.getList()); //on ajoute la liste d'objet dans laquelle le rapport ets déjà calculé.
-        Quicksort (listeObjet,0,listeObjet.size()-1); //on trie les objets
-        Iterator<Objet> tri = listeObjet.iterator(); //iterator permet de supprimer un élément de la collection
-        while(tri.hasNext() && sac.getPoids() < sac.getPoidsMaximal()){
-            sac.add(tri.next()); //on ajoute au sac si le poids actuel est inférieur au poids maximal
+
+public class Glouton{
+
+
+    public void resoudre(SacADos sac) {
+        ArrayList<Objet> listeobjets = sac.getList();
+        float poidsMax= sac.getPoidsMaximal();
+        Quicksort(listeobjets,0,listeobjets.size());
+
+        for (Objet objet: listeobjets){
+            if (sac.getPoids()+objet.getPoids()<=poidsMax){
+                sac.add(objet);
+            }
         }
-
     }
-
-
 
     //Algorithme de tri rapide
-    private void Quicksort(int [] tab, int entierPr, int entierDr){
+    private void Quicksort(List<Objet> tab, int entierPr, int entierDr){
         if(entierPr<entierDr){
-            int pivot = (entierDr- entierDr) / 2 + entierPr;
-             pivot=Répartition(tab,entierPr,entierDr,pivot);
-             Quicksort(tab,entierPr,pivot-1);
-             Quicksort(tab, pivot +1,entierDr);
+            int pivot = (entierPr + entierDr) / 2;
+            Répartition(tab,entierPr,entierDr,pivot);
+            Quicksort(tab,entierPr,pivot-1);
+            Quicksort(tab, pivot +1,entierDr);
         }
     }
 
-    private int Répartition(int[] tab, int entierPr, int entierDr,int pivot){
+    private Objet Répartition(List<Objet> tab, int entierPr, int entierDr, int pivot){
         Echanger(tab,pivot,entierDr);
+
         int j = entierPr;
-        for (int i= entierPr; i<entierDr-1; i++){
-            if(tab[i]<=tab[entierDr]){
+        for (int i= entierPr; i<=entierDr-1; i++){
+            if(tab.get(i).getRapport()<=tab.get(entierDr).getRapport()){
+                Echanger(tab,i,j);
                 j=j+1;
             }
         }
         Echanger(tab,entierDr,j);
-        return(tab[j]);
+        return(tab.get(j));
     }
 
-    public static void Echanger(int[] t, int entierDr, int pivot) {
+    public static void Echanger(List<Objet> t, int entierDr, int pivot) {
         //échange entre le pivot et le dernier du tableau (le pivot devient le dernier du tableau)
-        int tmp = t[pivot];
-        t[pivot] = t[entierDr];
-        t[entierDr] = tmp;
+        Objet tmp = t.get(entierDr);
+        t.set(entierDr, t.get(pivot));
+        t.set(pivot,tmp);
     }
+
 }
