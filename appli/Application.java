001f7c8284db1d134e4ab71422fc6651b9bd5a01
Meryem0207 <72689631+Meryem0207@users.noreply.github.com>
2021-10-02 13:52:31 +0200

    Update Application.java

--- appli/Application.java
+++ appli/Application.java
@@ -1,10 +1,15 @@
 package appli;
+import com.sun.xml.internal.txw2.output.SaxSerializer;
 import objet.Objet;
+import sac.SacADos;
+
 public class Application {
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args){
+        SacADos sacADos = null;
         String chemin = "scr/appli/items.txt";
-
+            sacADos = new SacADos(chemin,sacADos.getPoidsMaximal());
+    sacADos.resoudre();
 
     }
 }
