0668ec67924935063982145c178babcc29347c18
BLIZERTO <74712916+BLIZERTO@users.noreply.github.com>
2021-10-03 12:52:17 +0200

    Update Glouton.java

--- Methode/Glouton.java
+++ Methode/Glouton.java
@@ -12,9 +12,12 @@ public class Glouton{
 
 
     public void resoudre(SacADos sac) {
+
         ArrayList<Objet> listeobjets = sac.getList();
+        for (int i=0; i<listeobjets.size()-1; i++)
+            listeobjets.get(i).Rapport();
         float poidsMax= sac.getPoidsMaximal();
-        Quicksort(listeobjets,0,listeobjets.size());
+        Quicksort(listeobjets,0,listeobjets.size()-1);
 
         for (Objet objet: listeobjets){
             if (sac.getPoids()+objet.getPoids()<=poidsMax){
@@ -25,26 +28,26 @@ public class Glouton{
 
     //Algorithme de tri rapide
     private void Quicksort(List<Objet> tab, int entierPr, int entierDr){
-        if(entierPr<entierDr){
+        if(entierPr<entierDr) {
             int pivot = (entierPr + entierDr) / 2;
-            Répartition(tab,entierPr,entierDr,pivot);
+            pivot = Répartition(tab,entierPr,entierDr,pivot);
             Quicksort(tab,entierPr,pivot-1);
             Quicksort(tab, pivot +1,entierDr);
         }
     }
 
-    private Objet Répartition(List<Objet> tab, int entierPr, int entierDr, int pivot){
+    private int Répartition(List<Objet> tab, int entierPr, int entierDr, int pivot){
         Echanger(tab,pivot,entierDr);
 
         int j = entierPr;
         for (int i= entierPr; i<=entierDr-1; i++){
-            if(tab.get(i).getRapport()<=tab.get(entierDr).getRapport()){
+            if(tab.get(i).getRapport()>=tab.get(entierDr).getRapport()){
                 Echanger(tab,i,j);
                 j=j+1;
             }
         }
         Echanger(tab,entierDr,j);
-        return(tab.get(j));
+        return j;
     }
 
     public static void Echanger(List<Objet> t, int entierDr, int pivot) {
