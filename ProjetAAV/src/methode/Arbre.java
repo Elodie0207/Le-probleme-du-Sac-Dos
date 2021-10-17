5f1e4dcf2e6c496dcc943d0557d1e651fdd52f57
BLIZERTO <74712916+BLIZERTO@users.noreply.github.com>
2021-10-17 22:52:53 +0200

    Update Arbre.java

--- ProjetAAV/src/methode/Arbre.java
+++ ProjetAAV/src/methode/Arbre.java
@@ -4,35 +4,65 @@ import objet.Objet;
 
 import java.util.ArrayList;
 
+/**
+ * La classe Arbre crée un arbre
+ *
+ * @author Meryem Guldas & Sébastien Ramirez
+ */
 public class Arbre {
+    /**
+     * La valeur actuel
+     */
     private float valeur;
-    private Arbre fg; //fils supérieur
-    private Arbre fd;//fils inférieure
+
+    /**
+     * Le fils gauche (fils supérieur)
+     */
+    private Arbre fg;
+
+    /**
+     * Le fils droite (fils inférieur)
+     */
+    private Arbre fd;
+
+    /**
+     * La profondeur
+     */
     private final int profondeur;
+
+    /**
+     * Le pere
+     */
     private Arbre pere;
+
+    /**
+     * Le poid
+     */
     private float poids;
+
+    /**
+     * L'index de l'objet
+     */
     private int idx;
 
     /**
      * Constructeur d'arbre générant un arbre vide
-     *
      */
-    //Arbre vide
     public Arbre() {
         this.profondeur = 0;
         this.pere = this;
         this.poids=0;
         this.valeur=0;
-         idx=-1;
+        idx=-1;
     }
 
     /**
      * Contructeur d'arbre générant un arbre
      * Il est définit par son poids, ça valeur, par son pere, et son index (des objets)
-     * @param poids
-     * @param valeur
-     * @param pere
-     * @param idx
+     * @param poids le poids du noeud
+     * @param valeur la valeur du noeud
+     * @param pere le noeud pere
+     * @param idx l'index de l'objet
      */
     public Arbre(float poids, float valeur, Arbre pere, int idx) {
         this.poids = poids;
@@ -43,7 +73,7 @@ public class Arbre {
     }
 
     /**
-     *
+     * Renvoie le fils droit
      * @return fils droit
      */
     public Arbre getFd() {
@@ -51,19 +81,19 @@ public class Arbre {
     }
 
     /**
-     *
-     * @return l'index
-     */
-    public int getIdx() {return idx;}
-
-    /**
-     *
+     * Renvoie le fils gauche
      * @return fils gauche
      */
     public Arbre getFg(){ return fg;}
 
     /**
-     *
+     * Renvoie l'index de l'objet
+     * @return int l'index
+     */
+    public int getIdx() {return idx;}
+
+    /**
+     * Renvoie le pere
      * @return pere
      */
     public Arbre getPere() {
@@ -71,16 +101,16 @@ public class Arbre {
     }
 
     /**
-     *
-     * @return valeur
+     * Renvoie la valeur du noeud
+     * @return float valeur
      */
     public float getValeur() {
         return valeur;
     }
 
     /**
-     *
-     * @return poids
+     * Renvoie le poid du noeud
+     * @return float poids
      */
     public float getPoids() {
         return poids;
@@ -88,28 +118,29 @@ public class Arbre {
 
     /**
      * La profondeur de l'arbre
-     * @return profondeur
+     * @return int profondeur
      */
     public int getProfondeur(){
         return this.profondeur;
     }
 
     /**
-     * Permet de générer le fils Gauche avec un objet
-     * @param objet
-     * @param idx
-     */
-    public void setFg(Objet objet, int idx) {
-        fg = new Arbre(poids + objet.getPoids(), this.getValeur()+ objet.getPrix(), this, idx);
-    }
-
-    /**
-     *
+     * Verifie si un noeud est la racine
      * @return Profondeur
      */
     public boolean Racine(){
         return this.profondeur == 0;
     }
+
+    /**
+     * Permet de générer le fils Gauche avec un objet
+     * @param objet à ajouter
+     * @param idx l'index de l'objet
+     */
+    public void setFg(Objet objet, int idx) {
+        fg = new Arbre(poids + objet.getPoids(), this.getValeur()+ objet.getPrix(), this, idx);
+    }
+
     /**
      * Permet de générer le fils Droit sans objet
      */
